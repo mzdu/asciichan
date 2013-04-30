8afa5fe97f082d95d9eb26cb95c67ae6cabab43d
mzdu <dan.du.pub@gmail.com>
2013-04-30 12:15:34 -0400

    add changetuple.py a very interesting idea comes from MIT opencourse
    keyword: what if I wanna change Tuple....

---
+++ exercises/changetuple.py
@@ -0,0 +1,6 @@
+# if you simply use a + (5), there will be an error.
+# but if you use List for the whole idea, it will be better.
+# I think the professor in MIT messed up the Tuple and List
+
+a = (1,2,3,4)
+print a + (5,)
