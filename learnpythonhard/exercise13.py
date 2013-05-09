02ee7d78abe81ee6c9e2de73ca3fc3d24329cf8d
mzdu <dan.du.pub@gmail.com>
2013-05-08 21:02:27 -0400

    to 1.3 Python Cookbook

--- learnpythonhard/exercise13.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-script, first, second, third = argv
-
-print "The script is called:", script
-print "Your first variable is called:", first
-print "Your second variable is called:", second
-print "Your third variable is called:", third
-
---
+++ pythonCookbook/text1/3isAString.py
@@ -0,0 +1,6 @@
+# test if an object is a string, or whether the object is string-like
+
+def isAString(sth):
+    return isinstance(str, basestring)
+
+print isAString('')
