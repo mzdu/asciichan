2560c92f6a259f053062f598420db94c9150f1ea
Mingzhe Du <dan.du.pub@gmail.com>
2014-03-22 19:35:13 -0400

    started leetcode questions

--- leetcode/reversepolishenotation.py
+++ leetcode/reversepolishenotation.py
@@ -1,6 +1,17 @@
+"""
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+"""
+
 class Solution:
     # @param tokens, a list of string
     # @return an integer
+
     def evalRPN(self, tokens):
         operators = ['+','-','*','/']
         
@@ -10,25 +21,25 @@ class Solution:
             if token not in operators:
                 numbers.append(token)
             else:
-                number1 = int(numbers.pop())
-                number2 = int(numbers.pop())
+                number1 = float(numbers.pop())
+                number2 = float(numbers.pop())
                 if token == '+':
                     result = number2 + number1
                 elif token == '-':
                     result = number2 - number1
                 elif token == '*':
                     result = number2 * number1
+                    
                 elif token =='/':
                     result = number2 / number1
                 else:
                     pass
                 
-                numbers.append(str(result))
-                print numbers
+                result = int(result)
+                result = str(result)
                 
-        return int(numbers[0])
-
-
-lista = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-d = Solution()
-print d.evalRPN(lista)
+                numbers.append(result)
+        
+        fi = numbers[0]   
+        fi = int(fi)
+        return fi
