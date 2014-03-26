12b1bad17fbc23d10e24f6209cf6bf4df7db5d0a
Mingzhe Du <dan.du.pub@gmail.com>
2014-03-26 18:24:27 -0400

    aaa

--- draft.py
+++ draft.py
@@ -1,34 +1,10 @@
-class Solution:
-    # @param tokens, a list of string
-    # @return an integer
-    def evalRPN(self, tokens):
-        operators = ['+','-','*','/']
+def singleNumber(A):
+    while A:
+        number = A.pop()
+        if number in A:
+            A.pop(A.index(number))
+        else:
+            return number
         
-        numbers = []
-        
-        for token in tokens:
-            if token not in operators:
-                numbers.append(token)
-            else:
-                number1 = int(numbers.pop())
-                number2 = int(numbers.pop())
-                if token == '+':
-                    result = number2 + number1
-                elif token == '-':
-                    result = number2 - number1
-                elif token == '*':
-                    result = number2 * number1
-                elif token =='/':
-                    result = number2 / number1
-                else:
-                    pass
-                
-                numbers.append(str(result))
-                print numbers
-                
-        return int(numbers[0])
-
-
-lista = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-d = Solution()
-print d.evalRPN(lista)
+alist = [1,0,1]
+print singleNumber(alist)
