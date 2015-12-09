0f6c40c452b81e7d731b43cba9695c8150c6f244
mzdu <daniel_pub@hotmail.com>
2015-12-08 23:27:48 -0500

    added circular detection with DFS

---
+++ PythonAlgorithms/circulardetection.py
@@ -0,0 +1,34 @@
+class DFSResult:
+    def __init__(self):
+        self.parent = {}
+        self.start_time = {}
+        self.finish_time = {}
+        self.edges = {}
+        self.order = []
+        self.t = 0
+        
+        
+def dfs(g):
+    results = DFSResult()
+    for vertex in g.itervertices():
+        if vertex not in results.parent:
+            dfs_visit(g, vertex, results)
+    return results
+
+def dfs_visit(g, v, results, parent = None):
+    if n not in results.parent:
+        dfs_visit(g, n, results, v)
+    
+    elif n not in results.finish_time:
+        results.edges[(v, n)] = 'back'
+        
+    elif results.start_time[v] < results.start_time[n]:
+        results.edges[(v, n)] = 'forward'
+        
+    else:
+        results.edges[(v, n)] = 'cross'
+            
+    results.t += 1
+    results.finish_time[v] = results.t
+    results.order.append(v)
+    
